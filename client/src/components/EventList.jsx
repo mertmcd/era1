903902a1bc0ee7688c8ab9aa230b34bd24ae2fd2
mertmcd <merttdem@gmail.com>
2025-01-05 13:22:59 +0300

    add EventCard and EventList components for event display

---
+++ client/src/components/EventList.jsx
@@ -0,0 +1,18 @@
+import EventCard from "./EventCard";
+
+const EventList = ({ events }) => {
+  return (
+    <div className="event-list grid gap-6 sm:grid-cols-2 lg:grid-cols-3 p-6">
+      {events.map((event) => (
+        <EventCard
+          key={event.id}
+          title={event.title}
+          date={event.date}
+          description={event.description}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default EventList;
